d54f28e7c47404bf8cdee70e91bde7d08c2e60eb
Gabriel Nikol <gabriel-martin.nikol@stud.uni-bamberg.de>
2017-06-11 19:29:47 +0200

    print out descriptors

--- examples/discover/discover.c
+++ examples/discover/discover.c
@@ -26,55 +26,63 @@
 
 #include "gattlib.h"
 
-int main(int argc, char *argv[])
-{
-	gatt_connection_t* connection;
-	gattlib_primary_service_t* services;
-	gattlib_characteristic_t* characteristics;
-	int services_count, characteristics_count;
-	char uuid_str[MAX_LEN_UUID_STR + 1];
-	int ret, i;
+int main(int argc, char *argv[]) {
+    gatt_connection_t *connection;
+    gattlib_primary_service_t *services;
+    gattlib_characteristic_t *characteristics;
+    gattlib_descriptor_t *descriptors;
+    int services_count, characteristics_count, descriptor_count;
+    char uuid_str[MAX_LEN_UUID_STR + 1];
+    int ret, i;
 
-	if (argc != 2) {
-		printf("%s <device_address>\n", argv[0]);
-		return 1;
-	}
+    if (argc != 2) {
+        printf("%s <device_address>\n", argv[0]);
+        return 1;
+    }
 
-	connection = gattlib_connect(NULL, argv[1], BDADDR_LE_PUBLIC, BT_SEC_LOW, 0, 0);
-	if (connection == NULL) {
-		fprintf(stderr, "Fail to connect to the bluetooth device.\n");
-		return 1;
-	}
+    connection = gattlib_connect(NULL, argv[1], BDADDR_LE_PUBLIC, BT_SEC_LOW, 0, 0);
+    if (connection == NULL) {
+        fprintf(stderr, "Fail to connect to the bluetooth device.\n");
+        return 1;
+    }
 
-	ret = gattlib_discover_primary(connection, &services, &services_count);
-	if (ret != 0) {
-		fprintf(stderr, "Fail to discover primary services.\n");
-		return 1;
-	}
+    ret = gattlib_discover_primary(connection, &services, &services_count);
+    if (ret != 0) {
+        fprintf(stderr, "Fail to discover primary services.\n");
+        return 1;
+    }
 
-	for (i = 0; i < services_count; i++) {
-		gattlib_uuid_to_string(&services[i].uuid, uuid_str, sizeof(uuid_str));
+    for (i = 0; i < services_count; i++) {
+        gattlib_uuid_to_string(&services[i].uuid, uuid_str, sizeof(uuid_str));
 
-		printf("service[%d] start_handle:%02x end_handle:%02x uuid:%s\n", i,
-				services[i].attr_handle_start, services[i].attr_handle_end,
-				uuid_str);
-	}
-	free(services);
+        printf("service[%d] start_handle:%02x end_handle:%02x uuid:%s\n", i,
+               services[i].attr_handle_start, services[i].attr_handle_end,
+               uuid_str);
+    }
+    free(services);
 
-	ret = gattlib_discover_char(connection, &characteristics, &characteristics_count);
-	if (ret != 0) {
-		fprintf(stderr, "Fail to discover characteristics.\n");
-		return 1;
-	}
-	for (i = 0; i < characteristics_count; i++) {
-		gattlib_uuid_to_string(&characteristics[i].uuid, uuid_str, sizeof(uuid_str));
+    ret = gattlib_discover_char(connection, &characteristics, &characteristics_count);
+    if (ret != 0) {
+        fprintf(stderr, "Fail to discover characteristics.\n");
+        return 1;
+    }
+    for (i = 0; i < characteristics_count; i++) {
+        gattlib_uuid_to_string(&characteristics[i].uuid, uuid_str, sizeof(uuid_str));
 
-		printf("characteristic[%d] properties:%02x value_handle:%04x uuid:%s\n", i,
-				characteristics[i].properties, characteristics[i].value_handle,
-				uuid_str);
-	}
-	free(characteristics);
+        printf("characteristic[%d] properties:%02x value_handle:%04x uuid:%s\n", i,
+               characteristics[i].properties, characteristics[i].value_handle,
+               uuid_str);
+    }
+    free(characteristics);
 
-	gattlib_disconnect(connection);
-	return 0;
+    ret = gattlib_discover_desc(connection, &descriptors, &descriptor_count);
+    for (i = 0; i < descriptor_count; i++) {
+        gattlib_uuid_to_string(&descriptors[i].uuid, uuid_str, sizeof(uuid_str));
+        printf("characteristic[%d] value_handle:%04x uuid:%s\n", i,
+               descriptors[i].handle, uuid_str);
+    }
+    free(descriptors);
+
+    gattlib_disconnect(connection);
+    return 0;
 }
