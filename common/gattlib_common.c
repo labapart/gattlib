b8158f243cedd0756a048673e11609335b383564
Chris Thornton <cwgthornton@gmail.com>
2020-04-07 12:36:03 -0700

    Only pass pointer value as long type on 64 bit systems.
    When testing this on a RPI this causes issues, so we need to switch
    based on the architecture.

--- common/gattlib_common.c
+++ common/gattlib_common.c
@@ -61,7 +61,13 @@ void gattlib_call_notification_handler(struct gattlib_handler *handler, const uu
 
 		d_gstate = PyGILState_Ensure();
 
-		PyObject *arglist = Py_BuildValue("(sLIO)", uuid_str, data, data_length, handler->user_data);
+		const char* argument_string;
+		if (sizeof(void*) == 8) {
+			argument_string = "(sLIO)";
+		} else {
+			argument_string = "(sIIO)";
+		}
+		PyObject *arglist = Py_BuildValue(argument_string, uuid_str, data, data_length, handler->user_data);
 		PyEval_CallObject((PyObject *)handler->notification_handler, arglist);
 
 		PyGILState_Release(d_gstate);
