3dfaa875b6757cfda6a920ac44e752f4c6c1483d
Olivier Martin <olivier@labapart.com>
2020-04-09 11:39:45 +0200

    gattlib-py/examples: Introduce 'advertisement_data'

---
+++ gattlib-py/examples/advertisement_data/advertisement_data.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import argparse
+
+from gattlib import adapter
+
+parser = argparse.ArgumentParser(description='Gattlib BLE Advertising Data example')
+args = parser.parse_args()
+
+
+def on_discovered_ble_device(device, user_data):
+    advertisement_data, manufacturer_id, manufacturer_data = device.get_advertisement_data()
+    print("Device Advertisement Data: %s" % manufacturer_data)
+
+
+# Use default adapter
+default_adapter = adapter.Adapter()
+
+# Scan for 30 seconds
+default_adapter.open()
+default_adapter.scan_enable(on_discovered_ble_device, 0, notify_change=True)
