b61c03ae25f29d23941c96f1092dee57d3ef8a66
Olivier Martin <olivier@labapart.com>
2020-04-09 11:49:19 +0200

    gattlib-py/examples/advertisement_data: Fix typo

--- gattlib-py/examples/advertisement_data/advertisement_data.py
+++ gattlib-py/examples/advertisement_data/advertisement_data.py
@@ -16,6 +16,6 @@ def on_discovered_ble_device(device, user_data):
 # Use default adapter
 default_adapter = adapter.Adapter()
 
-# Scan for 30 seconds
+# Scan indefinitely
 default_adapter.open()
 default_adapter.scan_enable(on_discovered_ble_device, 0, notify_change=True)
