769a8e34b5890b8d4ecaa6045cb4202accddecfc
Olivier Martin <olivier@labapart.com>
2024-02-22 09:35:15 +0100

    ci: Generate Python package with cibuildwheel

--- ci/generate-python-package.sh
+++ ci/generate-python-package.sh
@@ -23,6 +23,10 @@ cp -r ${ROOT_PATH}/include ${gattlib_py_package_dir}/
 cp -r ${ROOT_PATH}/CMakeLists.txt ${gattlib_py_package_dir}/
 cp -r ${ROOT_PATH}/CrossCompilation.cmake ${gattlib_py_package_dir}/
 
+# Build script
+mkdir ${gattlib_py_package_dir}/ci/
+cp -r ${ROOT_PATH}/ci/install-bluez.sh ${gattlib_py_package_dir}/ci/
+
 # Create MANIFEST.in
 cat <<EOT >> MANIFEST.in
 graft common
@@ -34,13 +38,13 @@ include CrossCompilation.cmake
 EOT
 
 # Install requirements
-python3 -m pip install wheel
+python3 -m pip --disable-pip-version-check install cibuildwheel==2.16.5
 
 # Generate packages
 pushd ${gattlib_py_package_dir}
 
 # Binary package
-python3 setup.py bdist_wheel
+python3 -m cibuildwheel --output-dir dist
 
 # Source package
 python setup.py sdist
